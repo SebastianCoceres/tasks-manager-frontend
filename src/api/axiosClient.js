e70fd962719dedfc9c5b305ea41e02dad3219f97
Sebastian Coceres <sebastian.coceres11@gmail.com>
2023-05-20 18:59:02 +0200

    test backend api url

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -3,6 +3,7 @@ import queryString from "query-string";
 
 // const baseUrl = 'http://[::1]:5000/api/v1/';
 const baseUrl = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? 'http://[::1]:5000/api/v1/': 'http://srv-captain--tasks-manager-backend/api/v1/';
+console.log(baseUrl)
 const getToken = () => localStorage.getItem("token");
 
 const axiosClient = axios.create({
